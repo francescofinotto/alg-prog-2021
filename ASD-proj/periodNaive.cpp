59d182420c1999acf3776e4b65bf14b67df91989
francesco finotto <francescofinotto@hotmail.com>
2021-03-28 17:24:59 +0200

    edited project

--- ASD-proj/periodNaive.cpp
+++ ASD-proj/periodNaive.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 using namespace std;
 
+#define A 1000
+#define B 1.0647//859778233493
+
 bool searchPeriod(string str, int p)
 {
     for(int i=0; i<str.length()-p-1; i++)
@@ -28,6 +31,7 @@ int periodNaive(string str)
 
 int main()
 {
+    cout<<B<<endl;
     string str;
     cin >> str;
     int p = periodNaive(str);
